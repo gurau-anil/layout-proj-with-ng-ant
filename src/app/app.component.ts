603ec555a59a8183e6cab186f86921ae6c513818
gurau-anil <gurau.anil@gmail.com>
2025-02-25 23:28:06 -0700

    app-layout updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzLayoutModule, NzSiderComponent } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   isCollapsed = false;
+  @ViewChild('sidenav', { static: false }) sidenav!: NzSiderComponent;
+
+  triggered(data: boolean){
+    console.log(this.sidenav)
+  }
 }
