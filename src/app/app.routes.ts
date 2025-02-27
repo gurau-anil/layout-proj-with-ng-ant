a751a9021ecbaf1d5dc4737c13c3038ad7bc1ab0
gurau-anil <gurau.anil@gmail.com>
2025-02-27 00:03:25 -0700

    login page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 export const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: '/layout' },
   { path: '', loadChildren: () => import('./layout/layout.routes').then(m => m.LAYOUT_ROUTES) },
   // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
 ];
--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { WelcomeComponent } from '../pages/welcome/welcome.component';
 
 export const LAYOUT_ROUTES: Routes = [
-  { path: '', component: LayoutComponent },
+  { 
+    path: '', 
+    component: LayoutComponent,
+    children:[
+      {path:'dashboard', redirectTo:"dashboard/welcome"},
+      {path:'dashboard/welcome', component:WelcomeComponent}
+    ]
+   },
 ];
