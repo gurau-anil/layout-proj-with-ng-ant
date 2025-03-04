2b127f84f14d8f184daf471d5cb4af07bb59435a
gurau-anil <gurau.anil@gmail.com>
2025-03-03 21:35:17 -0700

    routes update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 
 export const routes: Routes = [
-  // { path: '', pathMatch: 'full', redirectTo: '/layout' },
+  { path: '', pathMatch: 'full', redirectTo: '/layout' },
   { path: '', loadChildren: () => import('./layout/layout.routes').then(m => m.LAYOUT_ROUTES) },
   // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
